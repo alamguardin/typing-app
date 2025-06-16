101882bc661f5d65e1268c8cecdf4faad10d6ab2
Alam Guardin <alamguardin@gmail.com>
2025-06-15 20:01:18 -0500

    feat: show split quote in span

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css';
 
+// Render Quote and Keyboard
 const keys = [
 	['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
 	['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
@@ -11,14 +12,17 @@ const quotes = [
 	'Si no luchamos, no podemos ganar',
 ];
 
-document.body.addEventListener('keydown', (e) => {
-	console.log(e);
-});
-
 const quoteElement = document.querySelector('.quote');
 const keyboardElement = document.querySelector('.keyboard');
 
-quoteElement.innerHTML = `<h2>${quotes[0]}</h2>`;
+const currentQuote = quotes[0].split('');
+let quoteFragment = '';
+
+for (const letter of currentQuote) {
+	quoteFragment += `<span>${letter}</span>`;
+}
+
+quoteElement.innerHTML = `<h2>${quoteFragment}</h2>`;
 
 let keyboardUIFragment = '';
 
@@ -33,3 +37,63 @@ for (const list of keys) {
 }
 
 keyboardElement.innerHTML += keyboardUIFragment;
+
+//Compare texts
+// const currentQuote = quotes[0].toLowerCase().split('');
+const currentWritting = [];
+
+// Activate pressed state
+const chars = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	' ',
+	'Backspace',
+];
+
+document.body.addEventListener('keydown', (e) => {
+	if (!chars.includes(e.key)) return;
+
+	if (e.key === ' ') {
+		currentWritting.push(e.key);
+		return;
+	}
+
+	if (e.key === 'Backspace') {
+		currentWritting.pop();
+		return;
+	}
+
+	const currentKey = document.querySelector(`#${e.key}`);
+
+	currentKey.classList.add('press');
+
+	setTimeout(() => {
+		currentKey.classList.remove('press');
+	}, 200);
+
+	currentWritting.push(e.key);
+});
